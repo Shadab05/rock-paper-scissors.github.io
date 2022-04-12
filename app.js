084a1dd111a8f9d228f5416c1a33e7ca3f930765
Mohd. Shadab <47297055+Shadab05@users.noreply.github.com>
2022-04-12 20:30:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const game = () =>
         });
         
         //Computer Options
-        const computerOptions = ['rock', 'paper', 'scissors'];
+        const computerOptions = ['Rock', 'Paper', 'Scissors'];
 
         options.forEach(option => {
       option.addEventListener("click", function() {
